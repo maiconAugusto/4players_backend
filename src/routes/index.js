0562b2fce5ecd19af0823df4ab4721fed065928a
Maicon Augusto <maicon.augusto.carvalho@hotmail.com>
2021-03-06 11:14:20 -0400

    apply middleware

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,6 +9,9 @@ import FavoriteClubController from '../app/controllers/favoriteClub';
 import FavoritePlayerController from '../app/controllers/favoritePlayers';
 import PlayerVideoController from '../app/controllers/playerVideos';
 import AuthenticationController from '../app/controllers/authentication';
+
+import middleware from '../middleware/authentication';
+
 import Storage from '../configs/multer';
 
 const upload = multer(Storage);
@@ -16,38 +19,38 @@ const routes = new Router();
 
 routes.post('/auth', AuthenticationController.store);
 
-routes.get('/accounts', AccountController.index);
+routes.get('/accounts', middleware, AccountController.index);
 routes.post('/account', AccountController.store);
-routes.get('/account/:id', AccountController.show);
+routes.get('/account/:id', middleware, AccountController.show);
 routes.put('/account/:id', AccountController.update);
 routes.delete('/account/:id', AccountController.remove);
 
-routes.get('/players', PlayerController.index);
-routes.get('/player/:id', PlayerController.show);
+routes.get('/players', middleware, PlayerController.index);
+routes.get('/player/:id', middleware, PlayerController.show);
 routes.post('/player', upload.single('file'), PlayerController.store);
 routes.put('/player/:id', upload.single('file'), PlayerController.update);
 routes.delete('/player/:id', PlayerController.remove);
 
-routes.get('/clubs', ClubController.index);
-routes.get('/club/:id', ClubController.show);
+routes.get('/clubs', middleware, ClubController.index);
+routes.get('/club/:id', middleware, ClubController.show);
 routes.post('/club', upload.single('file'), ClubController.store);
 routes.put('/club/:id', upload.single('file'), ClubController.update);
 routes.delete('/club/:id', ClubController.remove);
 
-routes.get('/favoritesclubs/:id', FavoriteClubController.index);
-routes.get('/favoritesclub/:id', FavoriteClubController.show);
+routes.get('/favoritesclubs/:id', middleware, FavoriteClubController.index);
+routes.get('/favoritesclub/:id', middleware, FavoriteClubController.show);
 routes.post('/favoritesclub', FavoriteClubController.store);
 routes.put('/favoritesclub/:id', FavoriteClubController.update);
 routes.delete('/favoritesclub/:id', FavoriteClubController.remove);
 
-routes.get('/favoritesplayers/:id', FavoritePlayerController.index);
-routes.get('/favoritesplayer/:id', FavoritePlayerController.show);
+routes.get('/favoritesplayers/:id', middleware, FavoritePlayerController.index);
+routes.get('/favoritesplayer/:id', middleware, FavoritePlayerController.show);
 routes.post('/favoritesplayer', FavoritePlayerController.store);
 routes.put('/favoritesplayer/:id', FavoritePlayerController.update);
 routes.delete('/favoritesplayer/:id', FavoritePlayerController.remove);
 
-routes.get('/playervideos', PlayerVideoController.index);
-routes.get('/playervideo/:id', PlayerVideoController.show);
+routes.get('/playervideos', middleware, PlayerVideoController.index);
+routes.get('/playervideo/:id', middleware, PlayerVideoController.show);
 routes.post('/playervideo', upload.single('file'), PlayerVideoController.store);
 routes.put('/playervideo/:id', upload.single('file'), PlayerVideoController.update);
 routes.delete('/playervideo/:id', PlayerVideoController.remove);
