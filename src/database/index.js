2daca14a16215e3839da1e383f1b29c2b18ed8ac
Maicon Augusto <maicon.augusto.carvalho@hotmail.com>
2021-03-07 01:10:48 -0400

    .env

--- src/database/index.js
+++ src/database/index.js
@@ -2,6 +2,6 @@ import Mongoose from 'mongoose';
 
 require('dotenv').config();
 
-const database = Mongoose.connect(process.env.MONG,
+const database = Mongoose.connect(process.env.MONGO,
   { useNewUrlParser: true, useUnifiedTopology: true });
 export default database;
