a043af9836716ea1476c68e59c8f9193a5c2de79
Maicon Augusto <maicon.augusto.carvalho@hotmail.com>
2021-03-16 08:29:51 -0300

    update player

--- src/app/models/playersAccount.js
+++ src/app/models/playersAccount.js
@@ -7,6 +7,10 @@ const players = Mongoose.Schema({
   profile: { type: 'string', required: false },
   profile_path: { type: 'string', required: false },
   playerHeight: { type: Number, required: false },
+  club: { type: 'string', required: false },
+  nationality: { type: 'string', required: false },
+  phone: { type: 'string', required: false },
+  dominantfoot: { type: 'string', required: false },
   country: { type: 'string', required: false },
   account: {
     type: Mongoose.Schema.Types.ObjectId,
