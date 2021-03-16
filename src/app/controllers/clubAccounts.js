6bf69d6a104ffe161a06f25881600426fa3a651b
Maicon Augusto <maicon.augusto.carvalho@hotmail.com>
2021-03-16 18:06:28 -0300

    logs error

--- src/app/controllers/clubAccounts.js
+++ src/app/controllers/clubAccounts.js
@@ -36,6 +36,7 @@ class ClubAccountsController {
       const data = await ClubAccountsModel(request.body).save();
       return response.status(200).json({ data });
     } catch (error) {
+      console.log(error);
       return response.status(400).json(error);
     }
   }
