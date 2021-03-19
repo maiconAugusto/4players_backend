2d15d4f6f857baea809a78a594502730f6a10a5f
Maicon Augusto <maicon.augusto.carvalho@hotmail.com>
2021-03-18 23:16:22 -0300

    logs success

--- src/app/controllers/searchPlayers.js
+++ src/app/controllers/searchPlayers.js
@@ -12,19 +12,21 @@ class SearchPlayersController {
 
       const filteredData = data
         .filter((item) => {
-          const age = item.playerAge.split('/');
-          const playerAgeYear = age[2];
+          if (item.playerAge !== undefined || item.country !== undefined || item.playerHeight) {
+            const age = item.playerAge.split('/');
+            const playerAgeYear = age[2];
 
-          const ageQueryInit = startDate.split('/');
-          const queryAgeYearInit = ageQueryInit[2];
+            const ageQueryInit = startDate.split('/');
+            const queryAgeYearInit = ageQueryInit[2];
 
-          const ageQueryEnd = endDate.split('/');
-          const queryAgeYearEnd = ageQueryEnd[2];
+            const ageQueryEnd = endDate.split('/');
+            const queryAgeYearEnd = ageQueryEnd[2];
 
-          return checkPositions(item.position, positions) === true && item.country === country
-          && playerAgeYear >= queryAgeYearInit && playerAgeYear <= queryAgeYearEnd
-          && item.playerHeight >= parseFloat(heightInit).toFixed(2)
-          && item.playerHeight <= parseFloat(heightEnd).toFixed(2);
+            return checkPositions(item.position, positions) === true && item.country === country
+            && playerAgeYear >= queryAgeYearInit && playerAgeYear <= queryAgeYearEnd
+            && item.playerHeight >= parseFloat(heightInit).toFixed(2)
+            && item.playerHeight <= parseFloat(heightEnd).toFixed(2);
+          }
         });
 
       return response.status(200).json({ data: filteredData });
