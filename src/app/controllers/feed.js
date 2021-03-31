4f6b42935332f9698e26aea562700040a86289aa
Maicon Augusto <maicon.augusto.carvalho@hotmail.com>
2021-03-31 10:35:54 -0300

    modify

--- src/app/controllers/feed.js
+++ src/app/controllers/feed.js
@@ -3,7 +3,7 @@ import VideoModel from '../models/playerVideos';
 class FeedController {
   async index(request, response) {
     try {
-      const data = (await VideoModel.find().populate('userId')).reverse();
+      const data = (await VideoModel.find().sort(1).populate('userId')).reverse();
       return response.status(200).json({ data });
     } catch (error) {
       return response.status(400).json({ error });
